39df2e18522e4267ff6ca1135bc4a0120c7ed606
TGRodriguez <tgrodriguez@fi.uba.ar>
2024-06-18 01:48:37 -0300

    feat: Added two exception handlers to the app, one for validation errors and another for custom exceptions

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 import uvicorn
 from configs.settings import settings
 from routers.dashboards import DashboardsRouter
+from routers.folders import FoldersRouter
 from configs.database import mongodb
 from contextlib import asynccontextmanager
+from fastapi.exceptions import RequestValidationError
+from errors import CustomException, handle_validation_error, handle_custom_exception
 
 
 @asynccontextmanager
@@ -17,6 +20,9 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(DashboardsRouter)
+app.include_router(FoldersRouter)
+app.add_exception_handler(RequestValidationError, handle_validation_error)
+app.add_exception_handler(CustomException, handle_custom_exception)
 
 
 if __name__ == "__main__":
