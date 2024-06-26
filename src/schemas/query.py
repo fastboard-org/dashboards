7701ff2f1968269afd112c77b60255a721c09702
TGRodriguez <tgrodriguez@fi.uba.ar>
2024-06-26 17:31:44 -0300

    feat: Added schemas for Query: Create, Update, Get and Response

---
+++ src/schemas/query.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+from typing import Optional
+from beanie import PydanticObjectId as ObjectId
+from pydantic import Field
+
+
+class QueryCreate(BaseModel):
+    name: str
+    user_id: int
+    connection_id: ObjectId
+    metadata: Optional[dict] = {}
+
+
+class QueryUpdate(BaseModel):
+    name: Optional[str] = None
+    metadata: Optional[dict] = None
+
+
+class QueriesGet(BaseModel):
+    user_id: Optional[int] = None
+    name: Optional[str] = None
+    connection_id: Optional[ObjectId] = None
+
+
+class QueryResponse(BaseModel):
+    id: ObjectId = Field(alias="_id")
+    name: str
+    user_id: int
+    connection_id: ObjectId
+    metadata: Optional[dict] = {}
+
+    class Config:
+        from_attributes = True
+        populate_by_name = True
